4e780d6a4866a6bb624ec7aa2f7433efe8929cf0
SunniM <57147145+SunniM@users.noreply.github.com>
2022-11-23 10:17:12 -0600

    Create topology.py

---
+++ topology.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from mininet.cli import CLI
+from mininet.net import Mininet
+from mininet.log import setLogLevel
+from mininet.topo import SingleSwitchTopp
+
+#Mininet topology with 3 hosts and 1 swtich
+def main()
+    setLogLevel('info')                 # Set log level to into to see more output
+    sst = SingleSwitchTopology(k=3)     # Create topoloywith 3 hosts
+    net = Mininet(sst)                  # Start network from topology
+    net.start()                         # Start network
+    CLI(net)                            # Start CLI to interact with network
+    net.stop()                          # Stop network
+  
+if __name__ == '__main__':
+    main()
